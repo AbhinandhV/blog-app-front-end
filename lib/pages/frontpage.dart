c7f89e41ecc88adba89a0fdb82ac6e37b2a4e905
AbhinandhV <abhinandh2255@gmail.com>
2024-02-12 11:50:48 +0530

    completed ui

---
+++ lib/pages/frontpage.dart
@@ -0,0 +1,45 @@
+import 'package:blogapp/pages/singup.dart';
+import 'package:flutter/material.dart';
+
+class frontpage extends StatefulWidget {
+  const frontpage({super.key});
+
+  @override
+  State<frontpage> createState() => _frontpageState();
+}
+
+class _frontpageState extends State<frontpage> {
+  TextEditingController username1=new TextEditingController();
+  TextEditingController password1=new TextEditingController();
+  @override
+  void signin()
+  {
+    print("USERNAME :"+username1.text);
+    print("PASSWORD :"+password1.text);
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body:Container(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: username1,
+              decoration: InputDecoration(labelText: "enter username",border: OutlineInputBorder()),),
+            SizedBox(height: 30,),
+            TextField(controller: password1,
+              decoration: InputDecoration(labelText: "enter password",border: OutlineInputBorder()),),
+            SizedBox(height: 30,),
+            ElevatedButton(onPressed:signin, child: Text("login")),
+            SizedBox(height: 30,),
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>singup()));
+            }, child: Text("new user"))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/singup.dart
@@ -0,0 +1,81 @@
+
+import 'package:flutter/material.dart';
+
+class singup extends StatefulWidget {
+  const singup({super.key});
+
+  @override
+  State<singup> createState() => _singupState();
+}
+
+class _singupState extends State<singup> {
+  TextEditingController name1=new TextEditingController();
+  TextEditingController age1=new TextEditingController();
+  TextEditingController phonenumber1=new TextEditingController();
+  TextEditingController address1=new TextEditingController();
+  TextEditingController pincode1=new TextEditingController();
+  TextEditingController email1=new TextEditingController();
+  TextEditingController password1=new TextEditingController();
+
+  void sendvalue()
+  {
+    print("Name"+name1.text);
+    print("Name"+age1.text);
+    print("Name"+phonenumber1.text);
+    print("Name"+address1.text);
+    print("Name"+pincode1.text);
+    print("Name"+email1.text);
+    print("Name"+password1.text);
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container(
+          padding: EdgeInsets.all(30),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(controller: name1,
+                  decoration: InputDecoration(labelText: "NAME",border: OutlineInputBorder()),),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: age1,
+                  decoration: InputDecoration(labelText: "AGE",border: OutlineInputBorder()),),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: phonenumber1,
+                  decoration: InputDecoration(labelText: "PHONE  NUMBER",border: OutlineInputBorder()),),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: address1,
+                  decoration: InputDecoration(labelText: "ADDRESS",border: OutlineInputBorder()),),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: pincode1,
+                  decoration: InputDecoration(labelText: "PINCODE",border: OutlineInputBorder()),),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: email1,
+                  decoration: InputDecoration(labelText: "EMAIL",border: OutlineInputBorder()),),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: password1,
+                  decoration: InputDecoration(labelText: "PASSWORD",border: OutlineInputBorder()),),
+                SizedBox(height: 30,),
+                ElevatedButton(onPressed:sendvalue, child: Text("SUBMIT")),
+                SizedBox(height: 30,),
+                ElevatedButton(onPressed: (){
+                  Navigator.pop(context);
+                }, child: Text("<-"))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
